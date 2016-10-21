422fa2231bb480b2e6352aaea4e37dcc76b4e3fa
wei112177 <103021009@live.asia.edu.tw>
2016-10-21 11:54:39 +0800

    ex繳交
    1.2題

--- src/ex/ex01_103021009.java
+++ src/ex/ex01_103021009.java
@@ -1,15 +1,26 @@
 ﻿package ex;
+
 /*
  * Topic: 寫一個程式讓使多個正整數(包含0)直到使用者輸入-1 時停止，同時要顯示這些數字 總合 及 平均。
  * Date: 2016/10/17
  * Author: 103021009 周永振老師
  */
 import java.util.*;
+
 public class ex01_103021009 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Scanner scn=new Scanner(System.in);
-
+		Scanner scn = new Scanner(System.in);
+		int i = 0;
+		int sum = 0;
+		int count = -1;
+		while (i != -1) {
+			count++;
+			sum += i;
+			i = scn.nextInt();
+		}
+		System.out.println(sum);
+		System.out.println((double)sum/count);
 	}
 }
--- src/ex/ex02_103021009.java
+++ src/ex/ex02_103021009.java
@@ -12,20 +12,28 @@ public class ex02_103021009 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
-		int n = scn.nextInt();
-		int m = scn.nextInt();
-		String str = "Y";
-		while()
-		for (int i = 1; i <= n; i++) {
+
+		boolean str = true;
+		char b;
+		System.out.println("請輸入 n 和 m ");
+		while (str) {
+			int n = scn.nextInt();
+			int m = scn.nextInt();
+			for (int i = 1; i <= n; i++) {
 				for (int j = 1; j <= m; j++) {
 					System.out.print(i + "*" + j + "=" + (i * j) + "\t");
 				}
 				System.out.println();
 			}
-		System.out.println("是否繼續:");
-		if (){
-			
+			System.out.println("是否繼續:");
+			b = scn.next().charAt(0);
+			if (b == 'Y') {
+				str = true;
+				System.out.println("請再次輸入 n 和 m ");
+			} else if (b == 'N') {
+				System.out.println("感謝使用");
+				break;
+			}
 		}
-		System.out.println("結束");
 	}
 }
